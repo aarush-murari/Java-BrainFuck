108c656c9820fb0f062416fc7c6ef645fe1c50df
Aarush Murari <aarush.murari@gmail.com>
2025-03-13 12:34:54 -0700

    Added multiline loop support and an instruction handler
    It works I guess.

--- BrainFuck.java
+++ BrainFuck.java
@@ -1,11 +1,18 @@
+package bfJava;
+
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
 public class BrainFuck {
     public static byte[] stream = new byte[30000];
     public static int dp = 0;
-    public static LinkedList<Character> instructions= new LinkedList<Character>();
+    public static LinkedList<Character> instructions = new LinkedList<Character>();
     public static int pc = 0;
+    public static int pos1 = 0;
+	public static int pos2 = 0;
+    
+    public static boolean shouldIPop = true;
+    
     public static void main(String[] args) {
     	Scanner scnr = new Scanner(System.in);
     	while(scnr.hasNextLine()) {
@@ -19,10 +26,14 @@ public class BrainFuck {
     	}
     	
     }
-    
+    public static void remove(int pos1, int pos2) {
+    	for (int i = pos1; i < pos2; i++) {
+    		//System.out.println(instructions);
+    		instructions.remove();
+    	}
+    }
     public static void interpret(String data) {//intentional by design.
-    	int pos1 = 0;
-    	int pos2 = 0;
+    	
     	int i = 0;
     	Scanner bfScnr = new Scanner(System.in);
     	while (i < data.length()) {
@@ -38,21 +49,36 @@ public class BrainFuck {
 	    			if (dp > (stream.length - 1)) {
 	    				dp = 0;
 	    			}
+	    			if (shouldIPop) {
+	    				instructions.remove(pc);
+	    			}
 	    			break;
 	    		case '<':
 	    			dp--;
 	    			if (dp < 0) {
 	    				dp = stream.length - 1;
 	    			}
+	    			if (shouldIPop) {
+	    				instructions.remove(pc);
+	    			}
 	    			break;
 	    		case '+':
 	    			stream[dp] += 1;
+	    			if (shouldIPop) {
+	    				instructions.remove(pc);
+	    			}
 	    			break;
 	    		case '-':
 	    			stream[dp] -= 1;
+	    			if (shouldIPop) {
+	    				instructions.remove(pc);
+	    			}
 	    			break;
 	    		case '.':
 	    			System.out.print((char)stream[dp]);
+	    			if (shouldIPop) {
+	    				instructions.remove(pc);
+	    			}
 	    			break;
 	    		case ',':
 					try {
@@ -64,15 +90,21 @@ public class BrainFuck {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+					if (shouldIPop) {
+	    				instructions.remove(pc);
+	    			}
 					break;
 	    		case '[':
 	    			pos1 = pc;
+	    			shouldIPop = false;
 	    			break;
 	    		case ']':
 	    			if (stream[dp] != 0) {
 	    				pc = pos1;
 	    				continue;
 	    			}
+	    			shouldIPop = true;
+	    			remove(pos1, pc);
 	    			break;
     		}
     		
